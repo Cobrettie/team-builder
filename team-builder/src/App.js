ad6a72895953434a41f50bea36ee420e28d4d16b
Cobrettie <cobrettiegarner3@gmail.com>
2019-09-11 20:09:36 -0400

    initial commit, create react app, add new branch

---
+++ team-builder/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Builder Form</h1>
+    </div>
+  );
+}
+
+export default App;
